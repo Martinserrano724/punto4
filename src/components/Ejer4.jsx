43a9c9c78f8b5441a9b03cc9e1e0dd6daf0b8c6f
Martinserrano724 <martinserrano724@gmail.com>
2023-05-23 13:49:09 -0300

    mejora en el codigo de ejercicio4

--- src/components/Ejer4.jsx
+++ src/components/Ejer4.jsx
@@ -1,40 +1,50 @@
-import React from 'react';
+import React from "react";
 
-import {Form ,Button} from 'react-bootstrap'
-import { useState } from 'react';
-import ListaTarea from './ListaTarea'
-
+import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
+import ListaTarea from "./ListaTarea";
 
 const Ejer4 = () => {
-const [tarea,setTarea] = useState('');
-const [tareaArray,setTareaArray]=useState([]);
+  const [tarea, setTarea] = useState("");
+  const [tareaArray, setTareaArray] = useState([]);
 
-function handleSubmit(){
-  setTareaArray([...tareaArray,tarea]);
- setTarea('');
+  function handleSubmit() {
+    setTareaArray([...tareaArray, tarea]);
+    setTarea("");
   }
-  const eliminarTarea =(nombreFiltrado)=>{
-    let tareasFiltradas=tareaArray.filter((itemTarea)=>itemTarea!=nombreFiltrado)
-    setTareaArray(tareasFiltradas);
-  }
- 
-  function onSubmit(e){e.preventDefault();}
-    return (
-        <div>
-        <Form onSubmit={onSubmit}   >
-            <Form.Group className="mb-5" controlId="formTarea">
-            <Form.Label>Tarea</Form.Label>
-            <Form.Control type="text" placeholder="ingrese una Tarea" onChange={(e)=> setTarea(e.target.value)}
-         value={tarea}
-           />
-            </Form.Group>
-            <Button className="btn btn-secondary" type="button" onClick={()=>handleSubmit()} >
-            Enviar Tarea
-            </Button>
-        </Form> 
-        <ListaTarea tareas={tareaArray} eliminar={eliminarTarea}></ListaTarea>
-    </div>
+  const eliminarTarea = (nombreFiltrado) => {
+    let tareasFiltradas = tareaArray.filter(
+      (itemTarea) => itemTarea != nombreFiltrado
     );
+    setTareaArray(tareasFiltradas);
+  };
+
+  function onSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <div>
+      <Form onSubmit={onSubmit}>
+        <Form.Group className="mb-5" controlId="formTarea">
+          <Form.Label>Tarea</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="ingrese una Tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
+        </Form.Group>
+        <Button
+          className="btn btn-secondary"
+          type="button"
+          onClick={() => handleSubmit()}
+        >
+          Enviar Tarea
+        </Button>
+      </Form>
+      <ListaTarea tareas={tareaArray} eliminar={eliminarTarea}></ListaTarea>
+    </div>
+  );
 };
 
 export default Ejer4;
